955d3d6917c8d2a8be1fa4c97583bd85cbb3a284
Marty Friedel <1491079+martyf@users.noreply.github.com>
2022-10-10 14:11:59 +1100

    Tweak to manual tag behaviour
    Signed-off-by: Marty Friedel <1491079+martyf@users.noreply.github.com>

--- src/Tags/Iconamic.php
+++ src/Tags/Iconamic.php
@@ -110,7 +110,10 @@ class Iconamic extends Tags
 
         $icon = null;
         if (is_array($this->context) && array_key_exists($handle, $this->context) || isset($this->context[$handle])) {
-            $icon = $this->context[$handle];
+	        // if the context is NOT a string (if its a string, treat it as a manual one)
+            if (!is_string($this->context[$handle])) {
+                $icon = $this->context[$handle];
+            }
         }
 
         // manual mode
